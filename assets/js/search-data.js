8c2059b5c41e4eff3d3f3e2f87f98a30d441eeff
rushikd27 <146467071+rushikd27@users.noreply.github.com>
2025-09-14 18:24:49 +0000

    Deploying to gh-pages from @ rushikd27/rushikd27.github.io@6a3325b92534b1506d37b79a84695c03417c49f0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,17 +18,10 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Curriculum Vitae of Rushik Desai - PhD candidate in Materials Engineering at Purdue University",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
           },
         },{id: "post-a-post-with-plotly-js",
         
